73b3ce851f6c4dde30efeffbf58e4e49a05d28f9
jesset27 <jessewillian0@gmail.com>
2026-05-27 18:09:29 -0300

    Primeiro Exercicio

---
+++ Exe01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Exercicio 1: Caixa Eletrônico");
+            System.Console.WriteLine("Digite um número desejado por favor:");
+            int valorDesejado = int.Parse(Console.ReadLine());
+            if (valorDesejado % 10 == 0)
+            {
+                System.Console.WriteLine("Notas de 10 liberadas");
+            }
+            else
+            {
+                System.Console.WriteLine("Valor não aceito (precisa ser múltiplo de 10)");
+            }
+        }
+    }
+}
