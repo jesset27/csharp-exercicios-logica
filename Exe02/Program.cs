de58d769257da0ddeb4b7f924ef5985e34332124
jesset27 <jessewillian0@gmail.com>
2026-05-27 18:24:56 -0300

    Exercicio 2

---
+++ Exe02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exe02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício 2: Validação de Login");
+            System.Console.WriteLine("Digite seu usuário por favor:");
+            string user = Console.ReadLine();
+            System.Console.WriteLine("Digite sua senha por favor:");
+            string password = Console.ReadLine();
+            if (user == "admin" && password == "1234")
+            {
+                System.Console.WriteLine("Acesso Concedido!");
+            }
+            else
+            {
+                System.Console.WriteLine("Acesso Negado!");
+            }
+        }
+    }
+}
