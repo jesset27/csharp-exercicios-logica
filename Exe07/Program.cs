b62bd3946cbdde200deafffbf73c05592eafe643
jesset27 <jessewillian0@gmail.com>
2026-05-28 14:47:44 -0300

    Exercicio 7

---
+++ Exe07/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Exe07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Exercício 7: Triângulo Válido");
+            System.Console.WriteLine("Digite o primeiro número:");
+            int numberOne = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Digite o segundo número:");
+            int numberTwo = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Digite o terceiro número:");
+            int numberThree = int.Parse(Console.ReadLine());
+
+            //3 4 10
+            if (numberOne < (numberTwo + numberThree))
+            {
+                if (numberTwo < (numberThree + numberOne))
+                {
+                    if (numberThree < (numberOne + numberTwo))
+                    {
+                        System.Console.WriteLine("Triangulo Valido");
+                    }else
+                    {
+                        System.Console.WriteLine("Triangulo Invalido");
+                    }
+                }else
+                {
+                    System.Console.WriteLine("Triangulo Invalido");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Triangulo Invalido");
+            }
+
+
+        }
+    }
+}
