3bc2307e4c566031ae38408a24cca9b7f546e997
jesset27 <jessewillian0@gmail.com>
2026-05-28 15:12:20 -0300

    Exercicio 8

---
+++ Exe08/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Exe08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Exercício 8: Ano Bissexto");
+            System.Console.WriteLine("Digite seu ano por favor:");
+            int year = int.Parse(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                if (year % 100 != 0)
+                {
+                    System.Console.WriteLine("É bissexto");
+                } else if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        System.Console.WriteLine("É bissexto");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Não é bissexto");
+                    }
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Seu número não é bissexto");
+            }
+        }
+    }
+}
